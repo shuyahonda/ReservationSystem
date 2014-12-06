0483a91006543000cc72662fdd0296e35cce0107
Shuya Honda <syv2357@gmail.com>
2014-12-06 13:51:25 +0900

    時間選択用のFragmentのUI作った

--- app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/controller/TimeDecisionFragment.java
+++ app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/controller/TimeDecisionFragment.java
@@ -24,7 +24,7 @@ public class TimeDecisionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_dision, container, false);
+        return inflater.inflate(R.layout.fragment_time_decision, container, false);
     }
 
 
