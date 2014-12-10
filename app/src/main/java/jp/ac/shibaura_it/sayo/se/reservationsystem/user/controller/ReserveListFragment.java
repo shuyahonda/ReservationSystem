9bd2fe5174361e16139a1d3fd4a559613b54abb2
Shuya Honda <syv2357@gmail.com>
2014-12-10 17:22:04 +0900

    CardLibrary追加

--- app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/controller/ReserveListFragment.java
+++ app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/controller/ReserveListFragment.java
@@ -7,8 +7,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import jp.ac.shibaura_it.sayo.se.reservationsystem.user.R;
+import jp.ac.shibaura_it.sayo.se.reservationsystem.user.view.ReserveDetailDialogFragment;
+import me.drakeet.materialdialog.MaterialDialog;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,12 +28,31 @@ public class ReserveListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reserve_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_reserve_list, container, false);
+        ButterKnife.inject(this,view);
+        return view;
     }
 
     @OnClick(R.id.dialogTestButton)
     public void OnClick(View view) {
-        ReserveDetailFragment fragment = new ReserveDetailFragment();
-        fragment.show(this.getFragmentManager(),"test");
+        MaterialDialog mMaterialDialog = new MaterialDialog(this.getActivity())
+                .setTitle("MaterialDialog")
+                .setMessage("Hello world!")
+                .setPositiveButton("OK", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //mMaterialDialog.dismiss();
+
+                    }
+                })
+                .setNegativeButton("CANCEL", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        //mMaterialDialog.dismiss();
+
+                    }
+                });
+
+        mMaterialDialog.show();
     }
 }
--- app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/view/ReserveDetailDialogFragment.java
+++ app/src/main/java/jp/ac/shibaura_it/sayo/se/reservationsystem/user/view/ReserveDetailDialogFragment.java
@@ -25,7 +25,7 @@ public class ReserveDetailDialogFragment extends DialogFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_reserve_detail_dialog2, container, false);
+        return inflater.inflate(R.layout.fragment_reserve_detail_dialog, container, false);
     }
 
 
